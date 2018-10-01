ea800317028bf2e18cb048a5090fa0f91e051d45
Lenin Godinez <lenin@michelada.io>
2018-10-01 16:11:39 -0500

    add guest user for current_user

--- app/controllers/concerns/set_source.rb
+++ app/controllers/concerns/set_source.rb
@@ -2,10 +2,10 @@ module SetSource
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_source 
+    before_action :set_source
   end
 
   def set_source
     session[:source] = params[:q] if params[:q]
   end
-end 
+end
